8db1f6b031274d3ea7f953acce6134b9529096ab
JakubDeco <jakub.deco@kosickaakademia.sk>
2020-11-11 10:46:43 +0100

    Adding Main snd RWFiles class

---
+++ src/sk/kosickaakademia/deco/files/Main.java
@@ -0,0 +1,8 @@
+package sk.kosickaakademia.deco.files;
+
+public class Main {
+    public static void main(String[] args) {
+        RWFiles reader=new RWFiles();
+        reader.copyFileTurnToUCase("resources/a.txt");
+    }
+}
